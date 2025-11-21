c3f3096bf266b4411b1af7761f196faaefd575c6
000973 <1752432628@qq.com>
2025-11-21 18:31:30 +0800

    提交例程

---
+++ 普通的GPIO口控制led蜂鸣器按键/尹光普 GPIO按键控制LED/3-4按键控制LED/User/main.c
@@ -0,0 +1,18 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "LED.h"
+
+ int main(void)
+{
+	LED_Init();
+	while(1)
+	{
+        LED1_ON();
+		LED2_OFF();
+		Delay_ms(500);
+		LED1_OFF();
+		LED2_ON();
+		Delay_ms(500);
+		
+	}
+}
